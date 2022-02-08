eb8aa44df836c1a6994695f0dbb39dc89feb4895
Muhammad Wajahat Shahid <55183024+Wajishahid10@users.noreply.github.com>
2022-02-09 00:02:44 +0500

    host

--- uet_test/uet_test/settings.py
+++ uet_test/uet_test/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ii@z-ixn-10wbbcew2vs)j@stgaw81js&t5=kd+1k!_m_t7_d8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://uet-test.herokuapp.com/']
 
 
 # Application definition
