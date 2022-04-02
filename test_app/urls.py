1fbd0d0182c4cebe96f0ed60a18b27ba39c7825a
Muhammad Wajahat Shahid <55183024+Wajishahid10@users.noreply.github.com>
2022-04-03 01:33:43 +0500

    Update urls.py

--- test_app/urls.py
+++ test_app/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('api/signup', views.SignUp),
     path('api/login', views.Login),
 
-    path('api/getLoginManagerFromUID<str:uid>', views.getLogin_Manager),
+    path('api/getLoginManagerFromUID/<str:uid>', views.getLogin_Manager),
     path('api/getTypeFromEmail/<str:mail>', views.getAccountTypeFromEmail),
 
     path('api/userinfo/<int:pk>', views.getUser),
